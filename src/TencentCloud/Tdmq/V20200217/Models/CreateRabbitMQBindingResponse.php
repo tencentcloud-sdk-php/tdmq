581d12678cbfeb15dbb302a065a6bef1dfee7b36
tencentcloudapi <tencentcloudapi@tencent.com>
2025-04-25 05:29:14 +0800

    release 3.0.1366

--- src/TencentCloud/Tdmq/V20200217/Models/CreateRabbitMQBindingResponse.php
+++ src/TencentCloud/Tdmq/V20200217/Models/CreateRabbitMQBindingResponse.php
@@ -20,8 +20,8 @@ use TencentCloud\Common\AbstractModel;
 /**
  * CreateRabbitMQBinding返回参数结构体
  *
- * @method string getInstanceId() 获取实例名称
- * @method void setInstanceId(string $InstanceId) 设置实例名称
+ * @method string getInstanceId() 获取实例 ID
+ * @method void setInstanceId(string $InstanceId) 设置实例 ID
  * @method string getVirtualHost() 获取vhost名称
  * @method void setVirtualHost(string $VirtualHost) 设置vhost名称
  * @method integer getBindingId() 获取路由关系Id
@@ -32,7 +32,7 @@ use TencentCloud\Common\AbstractModel;
 class CreateRabbitMQBindingResponse extends AbstractModel
 {
     /**
-     * @var string 实例名称
+     * @var string 实例 ID
      */
     public $InstanceId;
 
@@ -52,7 +52,7 @@ class CreateRabbitMQBindingResponse extends AbstractModel
     public $RequestId;
 
     /**
-     * @param string $InstanceId 实例名称
+     * @param string $InstanceId 实例 ID
      * @param string $VirtualHost vhost名称
      * @param integer $BindingId 路由关系Id
      * @param string $RequestId 唯一请求 ID，由服务端生成，每次请求都会返回（若请求因其他原因未能抵达服务端，则该次请求不会获得 RequestId）。定位问题时需要提供该次请求的 RequestId。
